99727fa1e2d9ab642bcb246685fdd87fd5cef2c5
Panagiotis <panthebig@yahoo.gr>
2021-11-18 23:33:46 +0200

    Working counting words

--- askisi3/script3.sh
+++ askisi3/script3.sh
@@ -1,6 +1,27 @@
 #cat test.txt | tr 'A-Z' 'a-z' | sed 's/--/ /g' | sed 's/[^a-z ]//g' | tr -s '[[:space:]]' '\n' | sort | uniq -c | sort -n | tail -n1
 #tr -c '[:alnum:]' '[\n*]' < test.txt | sort | uniq -c | sort -nr | head  -10
 
+parse=false
+filename="tmp.txt"
+
+while read line; do
+
+if [[ $line == *"*** END OF THIS PROJECT GUTENBERG"* ]]; then
+  break
+fi
 
 
-sed -e 's/[^[:alpha:]]/ /g' $1 | tr '\n' " " |  tr -s " " | tr " " '\n'| tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | nl | head -n $2
+
+if [ $parse = true ]; then
+  echo $line >> $filename
+fi
+
+if [[ $line == *"*** START OF THIS PROJECT GUTENBERG"* ]]; then
+  parse=true
+fi
+
+done < $1
+
+sed -e 's/[^[:alpha:]]/ /g' $filename | tr '\n' " " |  tr -s " " | tr " " '\n'| tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | nl | head -n $2
+
+rm $filename
